31e25428eb98e7c1677d7c255ed04cfc88e11034
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2024-04-21 13:49:56 +0530

    add pre installatiom

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,14 @@ const authRoute = require('./routes/auth.route.js')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 dotenv.config()
 mongoose.connect(process.env.MONGODB_CONNECTION).then(() => console.log('Database connected...')).catch((err) => {
     console.log(err);
 })
+
+const __dirnames = path.resolve();
 
 const app = express()
 app.use(bodyParser.json())
@@ -24,6 +27,11 @@ app.listen(5000, () => {
 app.use('/api/user', userRoute)
 app.use('/api/auth', authRoute)
 
+app.use(express.static(path.join(__dirnames, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirnames, 'frontend', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
